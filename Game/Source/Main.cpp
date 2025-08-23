e70d7a54bf392d63e90742a9e0879c11a05d09d3
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-08-23 12:26:02 +0900

    기본적인 게임 진행을 위한 윈도우 생성 및 루프 추가

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+#include <raylib.h>
+
 int main(int argc, char* argv[])
 {
+    const int screenWidth = 800;
+    const int screenHeight = 800;
+    InitWindow(screenWidth, screenHeight, "Snake");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) 
+    {
+        BeginDrawing();
+        {
+            ClearBackground(RAYWHITE);
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
 	return 0;
 }

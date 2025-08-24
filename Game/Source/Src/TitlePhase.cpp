1d1b519e34076326c50b571d09f210b0797a31ab
CHOIJIONE\choij <choijione02@gmail.com>
2025-08-25 08:08:24 +0900

    타이틀 페이즈에 종료 버튼 추가

--- Game/Source/Src/TitlePhase.cpp
+++ Game/Source/Src/TitlePhase.cpp
@@ -3,6 +3,8 @@
 #include "GameLog.h"
 #include "TitlePhase.h"
 
+#include <raygui.h>
+
 void TitlePhase::Tick(float deltaSeconds)
 {
     for (auto& tickActor : _tickActors)
@@ -26,21 +28,31 @@ void TitlePhase::Render()
 
 void TitlePhase::Enter()
 {
+    // 여기, UI 위치 및 크기 하드코딩된 거 외부로 빼는 것 고려해보기.
     Button* startButton = ActorManager::Get().Create<Button>(
-        Vector2{ 400.0f, 400.0f },
+        Vector2{ 300.0f, 400.0f },
         Vector2{ 200.0f, 50.0f  },
         [&]() { GAME_LOG_INFO("PRESS_START_BUTTON!"); },
         "START"
     );
 
+    Button* quitButton = ActorManager::Get().Create<Button>(
+        Vector2{ 300.0f, 480.0f },
+        Vector2{ 200.0f, 50.0f },
+        [&]() { GAME_LOG_INFO("PRESS_QUIT_BUTTON!"); },
+        "QUIT"
+    );
+
     _tickActors =
     {
         startButton,
+        quitButton,
     };
 
     _renderActors =
     {
         startButton,
+        quitButton,
     };
 }
 
